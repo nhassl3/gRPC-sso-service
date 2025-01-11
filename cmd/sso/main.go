4554d882a8aa72e22ac88ac1b0f68db0877661ce
nhassl3 <matvey.kvasov.05@mail.ru>
2025-01-11 17:59:20 +0300

    ✨ Connect config file

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log/slog"
 
+	"github.com/nhassl3/sso/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	
 }
