ec7cb4553296cda66165b553266fec644055f144
nhassl3 <matvey.kvasov.05@mail.ru>
2025-01-13 00:00:36 +0300

    🧵 Add graceful shutdown for service sso auth

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/nhassl3/sso/internal/app"
 	"github.com/nhassl3/sso/internal/config"
@@ -26,6 +28,16 @@ func main() {
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
 
 	application.GRPCServer.MustRun() // panic when erros occurs
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCServer.Stop()
+	log.Info("application stopped")
 }
 
 // setupLogger this function provide logger for service
